cad67288ec683080c99e591d3376232b5d4386b1
Hyeonmin Kim <khm970514@gmail.com>
2023-04-21 13:49:40 +0900

    update namespace (chime->chimesdkmeetings)

--- my-chime-server/routes/webrtc.js
+++ my-chime-server/routes/webrtc.js
@@ -4,49 +4,50 @@ const { v4: uuidv4 } = require('uuid');
 const asyncify = require('asyncify-express');
 
 var router = asyncify(express.Router());
+meetings = {};
 
 // 차임 미팅 세션 요청
 router.get('/chime-integration/meeting-session', async function(req, res, next) {
-  const chime = new AWS.Chime({ region: 'us-east-1' });
-  chime.endpoint = new AWS.Endpoint('https://chime.us-east-1.amazonaws.com');
-
   try {
-    // 존재하는 미팅 리스트 찾기
-    const meeting_list = await chime.listMeetings().promise();
-    const find_meeting = Array.from(meeting_list.Meetings).find(
-      (it) => it.ExternalMeetingId === req.query.room
-    );
-    console.log(find_meeting);
+    // 리전설정
+    const meetings_control_region = "us-east-1"
+    const meetings_media_region = "ap-northeast-2"
+    const chime_endpoint = "https://meetings-chime." + meetings_control_region + ".amazonaws.com"
 
-    const meetingsResult = await chime.listMeetings().promise();
+    // ChimeSDKMMEetings 사용하여 객체 생성
+    const chime = new AWS.ChimeSDKMeetings({ region: meetings_control_region });
+    // 엔드포인트 설정
+    chime.endpoint = new AWS.Endpoint(chime_endpoint);
 
-    // Can find a Meeting with a specific “external id” (aka, “room”)?
-    const foundMeeting = Array.from(meetingsResult.Meetings).find(
-      (it) => it.ExternalMeetingId === req.query.room
-    );
+    // 미팅 리스폰스 생성. but 이미 존재하는 미팅인경우를 파악해야함
+    var meeting_resonse = "";
+    if(req.query.room in meetings){
+      console.log("meeting is exist");
+      meeting_resonse = meetings[req.query.room];
+    } else {
+      console.log("meeting is not exist");
+      console.log("make new meeting");
+      // 새로운 미팅 생성
+      meeting_response = await chime.createMeeting({
+        ClientRequestToken : uuidv4(),
+        MediaRegion :  meetings_media_region,
+        ExternalMeetingId : req.query.room
+      }).promise();
+      meetings[req.query.room] = meeting_response;
+    }
 
-    // 존재하는 미팅 리스트가 없다면 새로 미팅 만들기
-    const created_meeting_response = !find_meeting &&
-    await chime.createMeeting({
-      ClientRequestToken : uuidv4(),
-      MediaRegion : 'ap-northeast-2',
-      ExternalMeetingId : req.query.room
-    }).promise();
+    // 미팅 리스트 출력
+    console.log("meetings : ", meetings);
 
-    // 존재하는 미팅 리스트가 있다면 그대로 사용하기
-    const meeting_response = find_meeting ? 
-    { Meeting : find_meeting } : created_meeting_response;
-
-    console.log("meeting response : ", meeting_response);
-
-    // 참가자 리스폰스 리턴하기
+    // 참가자 리스폰스 생성
     const attendee_response = await chime.createAttendee({
       MeetingId : meeting_response.Meeting.MeetingId,
       ExternalUserId : uuidv4()
     }).promise();
 
     console.log("attendee response : ", attendee_response);
-
+    
+    // front에 미팅리스폰스, 참가자 리스폰스 send
     res.send({ attendee_response, meeting_response });
   } catch (error) {
     console.log(error);
