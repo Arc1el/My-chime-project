9b7df46e1941e8321f221a90529495114dbec6ba
Hyeonmin Kim <khm970514@gmail.com>
2023-05-03 11:01:13 +0900

    update ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -26,23 +26,20 @@ module "ecs" {
   }
 
   services = {
-    terraform-mychime-backend = {
+    terraform-chime-backend = {
       cpu    = 1024
       memory = 4096
 
-      # Container definition(s)
       container_definitions = {
-
-        container = {
+        chime-backend = {
           cpu       = 512
           memory    = 1024
           essential = true
-          image     = "${module.ecr_backend.repository_arn}:latest"
+          image     = "759320821027.dkr.ecr.ap-northeast-2.amazonaws.com/hmkim-project-backend:latest"
           memory_reservation = 50
-
           port_mappings = [
             {
-              name          = "backend_port_mappings"
+              name          = "port3001"
               containerPort = 3001
               protocol      = "tcp"
             }
@@ -50,35 +47,23 @@ module "ecs" {
         }
       }
 
-      service_connect_configuration = {
-        namespace = "example"
-        service = {
-          client_alias = {
-            port     = 3001
-            dns_name = "ecs-sample"
-          }
-          port_name      = "ecs-sample"
-          discovery_name = "ecs-sample"
-        }
-      }
-
       load_balancer = {
         service = {
-          target_group_arn = module.alb_backend.target_group_arns
-          container_name   = "ecs-sample"
+          target_group_arn = module.alb_backend.target_group_arns[0]
+          container_name   = "chime-backend"
           container_port   = 3001
-        }
+        } 
       }
 
       subnet_ids = module.vpc.public_subnets
       security_group_rules = {
-        alb_ingress_3001 = {
+        alb_ingress_3000 = {
           type                     = "ingress"
           from_port                = 3001
           to_port                  = 3001
           protocol                 = "tcp"
           description              = "Service port"
-          source_security_group_id = module.ecs_sg_backend.security_group_id
+          cidr_blocks = ["0.0.0.0/0"]
         }
         egress_all = {
           type        = "egress"
@@ -95,22 +80,4 @@ module "ecs" {
     Environment = "Development"
     Project     = "Example"
   }
-}
-
-module "ecs_sg_backend" {
-  source = "terraform-aws-modules/security-group/aws"
-
-  name        = "terraform-ecs_sg_backend"
-  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 3001
-      to_port     = 3001
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
 }
