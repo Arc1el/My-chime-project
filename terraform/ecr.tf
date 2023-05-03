ccddb62c2b127b0c291208ee81e4dc2f5f96d725
Hyeonmin Kim <khm970514@gmail.com>
2023-05-03 11:01:36 +0900

    update ecr lifecycle policy

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -3,6 +3,24 @@ module "ecr_backend" {
 
   repository_name = "terraform-chime-backend"
 
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],    
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
   tags = {
     Terraform   = "true"
     Environment = "dev"
